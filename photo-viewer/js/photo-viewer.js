b9901da4ce76befdc1bba100e88933896730f73a
Roy Janechuti <roysahn@hotmail.com>
2021-12-08 22:44:01 -0800

    adds a new photo viewer project

---
+++ photo-viewer/js/photo-viewer.js
@@ -0,0 +1,22 @@
+var request;						// Latest image to be requested
+var $current;						// Image currently being shown
+var cache = {};						// Cache object
+var $frame = $('#photo-viewer');	// Container for image
+var $thumbs = $('.thumb');			// Container for thumbnails
+
+function crossfade($img) {			// Function to fade between images
+									// Pas in new image as parameter
+	if ($current) {					// If there is currently an image showing
+		$current.stop().fadeOut('slow');	// Stop animation and fade it out
+	}
+
+	$img.css({	// Set the CSS margins for the image
+		marginLeft: -$img.width() / 2, 	// Negative margin of half image's width
+		marginTop: -$img.height() / 2	// Negative margin of half image's height
+	});
+
+	$img.stop().fadeTo('slow', 1);		// Stop animation on new image & fade in
+
+	$current = $img;					// New image becomes current image
+
+}
