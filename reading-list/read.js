ecd4e0f982c42a9069d4bd96c879b662e948b698
Roy Janechuti <roysahn@hotmail.com>
2022-02-06 19:23:00 -0800

    Creates the deleteEntry functionality

--- reading-list/read.js
+++ reading-list/read.js
@@ -25,7 +25,14 @@ function addEntry(e) {
 	let t = document.querySelector('.tb')
 	t.innerHTML += `<tr><td>${id.value}</td><td>${titles.value}</td>
 		<td>${author.value}</td><td>${year.value}</td><td>${isbn.value}</td>
-		<td><a href="#">X</a></td></tr>`
+		<td><a class="delete" href="#">X</a></td></tr>`
+
+	let d = document.querySelectorAll('.delete');
+
+	for(let i = 0; i < d.length; i++) {
+		// console.log(d[i])
+		d[i].addEventListener('click', deleteEntry)
+	}
 	
 	for (let i = 0; i < f.length; i++) {
 		f[i].value = ''
@@ -33,3 +40,10 @@ function addEntry(e) {
 	
 }
 
+
+function deleteEntry(e) {
+	e.preventDefault()
+	tr = this.parentNode.parentNode
+	tbody = this.parentNode.parentNode.parentNode
+	tbody.removeChild(tr)
+}
