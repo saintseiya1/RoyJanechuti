bfb4bc30ad25484435fbf440caf39d5d78e106f5
Roy Janechuti <roysahn@hotmail.com>
2022-02-14 22:38:43 -0800

    Adds comments

--- reading-list/reading-list.js
+++ reading-list/reading-list.js
@@ -1,3 +1,4 @@
+// Create variables
 let add = document.querySelector('.add')
 let id = document.querySelector('.id')
 let titles = document.querySelector('.titles')
@@ -6,39 +7,49 @@ let year = document.querySelector('.year')
 let isbn = document.querySelector('.isbn')
 let field = document.querySelectorAll('.field')
 
+// adding entries
 let addEntry = (e)=> {
 	e.preventDefault()
+
+	// checks if entries are blank
 	if(checkIfBlank())
 		{
 			alert('One or more fields are blank!')
 			return
 		}
+
+	// renders the HTML for the entries
 	let t = document.querySelector('.tb')
 	t.innerHTML += `<tr><td>${id.value}</td><td>${titles.value}</td>
 		<td>${author.value}</td><td>${year.value}</td><td>${isbn.value}</td>
 		<td><a class="delete" href="#">X</a></td></tr>`
 
+	// adds a delete class for each entry
 	let d = document.querySelectorAll('.delete');
-
 	for(let i = 0; i < d.length; i++) {
 		// console.log(d[i])
 		d[i].addEventListener('click', deleteEntry)
 	}
 	
+	// resets the form values to blanks
 	for (let i = 0; i < field.length; i++) {
 		field[i].value = ''
 	}
 }
 add.addEventListener('click', addEntry)
 
+// deletes the entry
 function deleteEntry(e) {
 	e.preventDefault()
 	tr = this.parentNode.parentNode
 	tbody = this.parentNode.parentNode.parentNode
 	tbody.removeChild(tr)
 }
-		
-function checkIfBlank() {
+	
+// checks for blank values	
+// if value is blank, class of empty is added
+// if value exists, class of empty is removed
+let checkIfBlank = ()=> {
 	for(i = 0; i < field.length; i++) {
 		if(field[i].value == '') {
 			field[i].classList.add('empty')	
@@ -48,11 +59,13 @@ function checkIfBlank() {
 			}
 		}
 	}
+
+	// if any value is left blank, true is returned
 	if (id.value == '' ||
-	titles.value == '' ||
-	author.value == '' ||
-	year.value == '' ||
-	isbn.value == ''
+		titles.value == '' ||
+		author.value == '' ||
+		year.value == '' ||
+		isbn.value == ''
 	)
 		{
 			return true
