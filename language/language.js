202d8fa2ab4820021845b791061f012a9805d107
Roy Janechuti <roysahn@hotmail.com>
2022-03-03 14:48:37 -0800

    Creates a viewer

--- language/language.js
+++ language/language.js
@@ -1,16 +1,27 @@
 let m = document.querySelectorAll('.mightyglyph');
-console.log(m)
-function enlarge() {
+let viewer = document.querySelector('#viewer');
 
-	this.classList.add('test')
-}
-function decrease() {
-
-	this.classList.remove('test');
-}
+// console.table(v.parentElement)
 
 for (var i = 0; i < m.length; i++) {
-	m[i].addEventListener('mouseover', enlarge);
-	m[i].addEventListener('mouseleave', decrease);
+	m[i].addEventListener('mouseover', show);
+	m[i].addEventListener('mouseleave', hide);
+
+}
+
+function show() {
+	// select the table row
+	let tableRow = this.parentElement
+	// select the character text
+	let characterText = tableRow.childNodes[1].innerText
+	// select the character symbol
+	let characterSymbol = this.innerText
+	// output the symbol and text to the viewer
+	viewer.innerHTML = characterSymbol + "<br>" + characterText
+}
+
+function hide() {
+	// clear the viewer
+	viewer.innerText = ''
 
 }
