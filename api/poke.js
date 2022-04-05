f55a845028c23dd4c509f530e851925d9d27163d
Roy Janechuti <roysahn@hotmail.com>
2022-04-04 21:30:46 -0700

    Adds the initial files for poke api project

---
+++ api/poke.js
@@ -0,0 +1,96 @@
+$(function () {
+    var pokemonSearch
+    var defaultPokemon = '1'
+    var defaultPokemonData
+
+    var initFunc = function () {
+        // https://pokeapi.co/api/v2/pokemon/?limit=898
+        defaultPokemonData = $.ajax({
+            url: "https://pokeapi.co/api/v2/pokemon/" + defaultPokemon,
+            method: "GET",
+        })
+
+        defaultPokemonData.done(function (data) {
+            defaultPokemonData = data
+            $('.loading-container').removeClass('active')
+            $('.pokedex h3').text(data.name.toUpperCase())
+            $('.poke-img img').attr('src', data.sprites.front_default)
+            // console.log(data)
+        })
+
+        defaultPokemonData.fail(function (jqXHR, textStatus, error) {
+            alert("Request failed: " + textStatus + ' ' + error)
+        })
+    }
+
+    initFunc()
+
+    $('.btn1').on('click', function () {
+        pokemonSearch = $('.pokedex input[type="text"]').val()
+
+        var request = $.ajax({
+            url: "https://pokeapi.co/api/v2/pokemon/" + pokemonSearch,
+            method: "GET"
+        })
+
+        request.done(function (data) {
+            $('.pokedex h3').text(data.name.toUpperCase())
+            $('.poke-img img').attr('src', data.sprites.front_default)
+        })
+
+        request.fail(function (jqXHR, textStatus, error) {
+            alert("Request failed: " + textStatus + ' ' + error)
+        })
+
+    })
+
+    $('.btn2').on('click', function () {
+        out = $('.pokedex input[type="text"]').val()
+
+        var request = $.ajax({
+            url: "https://pokeapi.co/api/v2/pokemon/?limit=898",
+            method: "GET"
+        })
+
+        request.done(function (data) {
+            let mylist = '';
+            for (x in data.results) {
+                mylist += '<option value="' + data.results[x].url + '">' +
+                    data.results[x].name + '</option>'
+            }
+            $('.output').append(mylist)
+        })
+
+        request.fail(function (jqXHR, textStatus, error) {
+            alert("Request failed: " + textStatus + ' ' + error)
+        })
+
+    })
+
+    $('.btn3').on('click', function (e) {
+        e.preventDefault()
+        poke = $('select').val()
+        var request = $.ajax({
+            url: poke,
+            method: "GET"
+        })
+        console.log(request)
+
+        request.done(function (data) {
+            $('.pokedex h3').text(data.name.toUpperCase())
+            $('.poke-img img').attr('src', data.sprites.front_default)
+        })
+
+        request.fail(function (jqXHR, textStatus, error) {
+            alert("Request failed: " + textStatus + ' ' + error)
+        })
+    })
+
+
+    //     pokemonSearch = $('.pokedex input[type="text"]').val()
+
+
+
+    // })
+
+})
