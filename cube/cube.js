5e418f2a6e81b2748dac8073fffc771331234fca
Roy Janechuti <roysahn@hotmail.com>
2022-04-20 21:31:50 -0700

    Adds a background image

--- cube/cube.js
+++ cube/cube.js
@@ -1,3 +1,4 @@
+
 let nameDiv = document.querySelector('.name')
 
 let fullName = ['R','o','y',' ','J','a','n','e','c','h','u','t','i']
@@ -5,10 +6,10 @@ let fullName = ['R','o','y',' ','J','a','n','e','c','h','u','t','i']
 let letterSpan = []
 
 for(x in fullName){
+	
 	letterSpan[x] = document.createElement('span')
 	letterSpan[x].classList.add('letter')
 	letterSpan[x].innerHTML = fullName[x]
-	
-	nameDiv.append(letterSpan[x])
-	}
+	nameDiv.append(letterSpan[x])	
+}
 
